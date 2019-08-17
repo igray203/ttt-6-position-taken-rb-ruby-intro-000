7f4e35eb1f4b24b2a17f2eeb38b2e193e00bff74
ian gray <igrayscale@gmail.com>
2019-08-17 21:00:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,9 @@
 board = ["","","","","","","","",""]
 
 def position_taken?(array,index)
-  if array[index] == "" || " " || nil
+  if array[index] == "" || array[index] == " " || array[index] == nil
     return false
-  elsif array[index] == "X" || "O"
+  elsif array[index] == "X" || array[index] == "O"
     return true
   end
 end
